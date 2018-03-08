a1945e7d706d4688a7014b95200c1eda90232074
Dillon Adams <dillon-a@hotmail.com>
2018-03-07 22:18:03 -0600

    reduce duplication and using static locking object to deal with multiple instances of the logging class using the same file

--- EZLogger.File/FileWriter.cs
+++ EZLogger.File/FileWriter.cs
@@ -5,20 +5,20 @@ namespace EZLogger.File
     public class FileWriter : IWriter
     {
         private readonly IFormatter<string> _Formatter;
-        private readonly object _LockObj;
+        private static object _LockObj;
 
         public string FileName { get; }
 
-        public FileWriter(string fileName)
-        {
-            _Formatter = new DefaultFileMessageFormatter();
-            FileName = fileName;
-        }
+        public FileWriter(string fileName) : this(new DefaultFileMessageFormatter(), fileName) { }
 
         public FileWriter(IFormatter<string> formatter, string fileName)
         {
             _Formatter = formatter;
-            _LockObj = new object();
+
+            if (_LockObj == null)
+            {
+                _LockObj = new object();
+            }
 
             FileName = fileName;
         }
