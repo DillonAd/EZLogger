0303d62cab77245f91d6f89e9cadc844c61f7a70
dillon <28228453+DillonAd@users.noreply.github.com>
2019-01-20 00:48:27 -0600

    Fixing missed copypasta mistakes

--- EZLogger.File/FileLoggerExtensions.cs
+++ EZLogger.File/FileLoggerExtensions.cs
@@ -5,10 +5,10 @@ namespace EZLogger.File
     public static class FileLoggerExtensions
     {
         public static IServiceCollection AddFileLogger<TFormatter>(this IServiceCollection serviceCollection) 
-            where TFormatter : IFormatter<string> => 
+            where TFormatter : class, IFormatter<string> => 
                 serviceCollection.AddScoped<ILogger, Logger>()
                                  .AddSingleton<IWriter, FileWriter>()
-                                 .AddSingleton<IFormatter<string>, DefaultFileMessageFormatter>();
+                                 .AddSingleton<IFormatter<string>, TFormatter>();
 
         public static IServiceCollection AddFileLogger(this IServiceCollection serviceCollection) => 
             AddFileLogger<DefaultFileMessageFormatter>(serviceCollection);
