0823113ac62be70a0a743921c4af6648d85cd718
dillon <28228453+DillonAd@users.noreply.github.com>
2019-01-20 17:32:44 -0600

    Adding Console logger

---
+++ EZLogger.Console/DefaultConsoleMessageFormatter.cs
@@ -0,0 +1,26 @@
+﻿using EZLogger;
+using System;
+using System.Text;
+
+namespace EZLogger.Console
+{
+    public class DefaultConsoleMessageFormatter : Formatter<string>
+    {
+        public override string FormatMessage(LogMessage message)
+        {
+            var sb = new StringBuilder();
+
+            if (message != null)
+            {
+                sb.Append(message.Date.ToLongDateString()).Append(" ")
+                    .Append(message.Date.ToLongTimeString()).Append(" - ")
+                    .Append(Enum.GetName(typeof(LogLevel), message.Level)).Append(" - ")
+                    .Append(message.Message).Append(Environment.NewLine)
+                    .Append(GetExceptionDetails(message.Exception))
+                    .Append(Environment.NewLine).Append(Environment.NewLine);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
