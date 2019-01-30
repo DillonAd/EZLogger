d8b5d4b8779ef2371b9fb38b2d347e8ff739a61a
dillon <28228453+DillonAd@users.noreply.github.com>
2019-01-30 17:19:15 -0600

    Adding parameter so the log file name can be specified through the extension methods

--- EZLogger.File/FileLoggerExtensions.cs
+++ EZLogger.File/FileLoggerExtensions.cs
@@ -4,13 +4,13 @@ namespace EZLogger.File
 {
     public static class FileLoggerExtensions
     {
-        public static IServiceCollection AddFileLogger<TFormatter>(this IServiceCollection serviceCollection) 
+        public static IServiceCollection AddFileLogger<TFormatter>(this IServiceCollection serviceCollection, string logFileName) 
             where TFormatter : class, IFormatter<string> => 
                 serviceCollection.AddScoped<ILogger, Logger>()
-                                 .AddSingleton<IWriter, FileWriter>()
+                                 .AddSingleton<IWriter>(new FileWriter(logFileName))
                                  .AddSingleton<IFormatter<string>, TFormatter>();
 
-        public static IServiceCollection AddFileLogger(this IServiceCollection serviceCollection) => 
-            AddFileLogger<DefaultFileMessageFormatter>(serviceCollection);
+        public static IServiceCollection AddFileLogger(this IServiceCollection serviceCollection, string logFileName) => 
+            AddFileLogger<DefaultFileMessageFormatter>(serviceCollection, logFileName);
     }
 }
