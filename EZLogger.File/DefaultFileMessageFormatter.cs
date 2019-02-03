5b586e24993e5dc48a1b8816441e303bf0429f3d
dillon <28228453+DillonAd@users.noreply.github.com>
2019-02-02 20:56:47 -0600

    Adding formatting for when the LogMessage object does not have an exception

--- EZLogger.File/DefaultFileMessageFormatter.cs
+++ EZLogger.File/DefaultFileMessageFormatter.cs
@@ -13,11 +13,17 @@ namespace EZLogger.File
             if (message != null)
             {
                 sb.Append(message.Date.ToLongDateString()).Append(" ")
-                    .Append(message.Date.ToLongTimeString()).Append(" - ")
-                    .Append(Enum.GetName(typeof(LogLevel), message.Level)).Append(" - ")
-                    .Append(message.Message).Append(Environment.NewLine)
-                    .Append(GetExceptionDetails(message.Exception))
-                    .Append(Environment.NewLine).Append(Environment.NewLine);
+                  .Append(message.Date.ToLongTimeString()).Append(" - ")
+                  .Append(Enum.GetName(typeof(LogLevel), message.Level)).Append(" - ")
+                  .Append(message.Message);
+                
+                if(message.Exception != null)
+                {
+                    sb.Append(Environment.NewLine)
+                      .Append(GetExceptionDetails(message.Exception));
+                }
+
+                sb.Append(Environment.NewLine);
             }
 
             return sb.ToString();
