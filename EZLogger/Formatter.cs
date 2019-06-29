d1fe84c7488f07a67b6abe69ea3f8ab2b981cb2b
dillon <28228453+DillonAd@users.noreply.github.com>
2019-06-28 20:18:19 -0500

    Removing outdated comment and changing accesibility of the constructor in the Formatter class

--- EZLogger/Formatter.cs
+++ EZLogger/Formatter.cs
@@ -8,7 +8,7 @@ namespace EZLogger
     {
         protected readonly Dictionary<LogLevel, string> LogLevels;
 
-        public Formatter()
+        protected Formatter()
         {
             if(LogLevels == null)
             {
@@ -23,10 +23,6 @@ namespace EZLogger
 
         public abstract T FormatMessage(LogMessage message);
 
-        // This method uses string.Format instead of $ interpolation
-        // to be backwards compatible with previous versions of C#.
-        // Once the dollar sign interpolation method is ubiquitous
-        // this can be changed to be more concise.
         protected string GetExceptionDetails(Exception ex) =>
             ex == null ? string.Empty : $"{ex.Message}\n{ex.StackTrace}\n\n{GetExceptionDetails(ex.InnerException)}";
     }
