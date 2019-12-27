4648ae9618481f114bee0e951a1473c324e69d19
Dillon Adams <28228453+DillonAd@users.noreply.github.com>
2019-12-26 21:24:30 -0600

    Removing unnecessary using statements

--- EZLogger.Console/DefaultConsoleMessageFormatter.cs
+++ EZLogger.Console/DefaultConsoleMessageFormatter.cs
@@ -1,6 +1,4 @@
-﻿using EZLogger;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Text;
 
 namespace EZLogger.Console
--- EZLogger/LogLevel.cs
+++ EZLogger/LogLevel.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace EZLogger
 {
     public enum LogLevel
